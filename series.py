7ff999ea2c819dd256f13a1060d156049828796b
Bradley Hower <bradleyhower@proton.me>
2024-01-09 14:01:28 -0800

    first commit

---
+++ series.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+  """A series of numbers where the subsequent number is the sum of the prior two. Begins with zero. By definition, Fibonacci at index 0 is 0 and fibonacci at index 1 is 1."""
+
+
+def lucas(n):
+    """A series of numbers where the subsequent number is the sum of the prior two. Begins with two. By definition, Fibonacci at index 0 is 2 and fibonacci at index 1 is 1."""
+
+def sum_series(n, first=0, second=1):
+   
