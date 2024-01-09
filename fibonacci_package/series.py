49b4a7f38196ec87b1146809772472a62b895613
Bradley Hower <bradleyhower@proton.me>
2024-01-09 15:10:23 -0800

    all pass

--- fibonacci_package/series.py
+++ fibonacci_package/series.py
@@ -1,12 +1,45 @@
 def fibonacci(n):
-  """A series of numbers where the subsequent number is the sum of the prior two. Begins with zero. By definition, Fibonacci at index 0 is 0 and fibonacci at index 1 is 1. To resolve, recusion is required."""
+  """A series of numbers where the subsequent number is the sum of the prior two. Begins with zero. By definition, Fibonacci at index 0 is 0 and fibonacci at index 1 is 1. To resolve, recusion is required.
+  ex: fibonacci(0) = 0
+      fibonacci(1) = 1
+      fibonacci(2) = fibonacci(1) + fibonacci(0) = 1 + 0 = 1
+      fibonacci(3) = fibonacci(2) + fibonacci(1) = 1 + 1 = 2
+      fibonacci(7) = fibonacci(6) + fibonacci(5) = 8 + 5 = 13
+  """
+  if n < 0:
+     return "Sorry, only positives"
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fibonacci(n-1) + fibonacci(n-2)
   
+def lucas(n):
+  """A series of numbers where the subsequent number is the sum of the prior two. Begins with two. By definition, Fibonacci at index 0 is 2 and fibonacci at index 1 is 1. To resolve, recusion is required.
+    ex: lucas(0) = 2
+    lucas(1) = 1
+    lucas(2) = lucas(1) + lucas(0) = 1 + 2 = 3
+    lucas(3) = lucas(2) + lucas(1) = 3 + 1 = 4
+    lucas(7) = lucas(6) + lucas(5) = 18 + 11 = 29
+  """
+  if n < 0:
+     return "Sorry, only positives"
+  if n == 0:
+    return 2
+  if n == 1:
+    return 1
+  return lucas(n-1) + lucas(n-2)
 
 
 
 
-# def lucas(n):
-#     """A series of numbers where the subsequent number is the sum of the prior two. Begins with two. By definition, Fibonacci at index 0 is 2 and fibonacci at index 1 is 1. To resolve, recusion is required."""
+def sum_series(n, first=0, second=1):
+  if n < 0:
+    return "Sorry, only positives"
+  if n == 0:
+    return first
+  if n == 1:
+    return second
+  return sum_series(n-1, first, second) + sum_series(n-2, first, second)
 
-# def sum_series(n, first=0, second=1):
-   
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,8 @@
 from fibonacci_package.series import fibonacci
+from fibonacci_package.series import lucas
+from fibonacci_package.series import sum_series
+
+## Fibonacci Tests
 
 def test_fibonacci_zero():
   actual = fibonacci(0)
@@ -15,7 +19,73 @@ def test_fibonacci_two():
   expected = 1
   assert actual == expected
 
-def test_fibonacci_eight():
-  actual = fibonacci(8)
+def test_fibonacci_seven():
+  actual = fibonacci(7)
   expected = 13
+  assert actual == expected
+
+## Lucas Tests
+
+def test_lucas_zero():
+  actual = lucas(0)
+  expected = 2
+  assert actual == expected
+
+def test_lucas_one():
+  actual = lucas(1)
+  expected = 1
+  assert actual == expected
+
+def test_lucas_two():
+  actual = lucas(2)
+  expected = 3
+  assert actual == expected
+
+def test_lucas_seven():
+  actual = lucas(7)
+  expected = 29
+  assert actual == expected
+
+## sum_series defaults
+  
+def test_sum_series_default_zero():
+  actual = sum_series(0)
+  expected = 0
+  assert actual == expected
+
+def test_sum_series_default_one():
+  actual = sum_series(1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_default_two():
+  actual = sum_series(2)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_default_seven():
+  actual = sum_series(7)
+  expected = 13
+  assert actual == expected
+
+## sum_series options 2 and 1
+  
+def test_sum_series_options_zero():
+  actual = sum_series(0, 2, 1)
+  expected = 2
+  assert actual == expected
+
+def test_sum_series_options_one():
+  actual = sum_series(1, 2, 1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_options_two():
+  actual = sum_series(2, 2, 1)
+  expected = 3
+  assert actual == expected
+
+def test_sum_series_options_seven():
+  actual = sum_series(7, 2, 1)
+  expected = 29
   assert actual == expected
